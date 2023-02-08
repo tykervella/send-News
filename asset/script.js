39b1c33fdfd3d68a6d5bddfcd0329e96046d761a
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-02-07 19:40:52 -0800

    lined up code

--- asset/script.js
+++ asset/script.js
@@ -17,85 +17,85 @@ var categorystring = "category=" + category + "&"
 
 // url to show top headlines in specific categories
 var topheadlinesurl = "https://newsapi.org/v2/top-headlines?" +
-          countrystring +
-          categorystring +
-          searchstring +
-          "apiKey=9db76ac16d454b918d7c994623816b9b"
+  countrystring +
+  categorystring +
+  searchstring +
+  "apiKey=9db76ac16d454b918d7c994623816b9b"
 
 var req = new Request(topheadlinesurl);
 
 // grab the API url and manipulates the data 
 fetch(req)
-    .then(function(response) {
-        return response.json();
-    })
+  .then(function (response) {
+    return response.json();
+  })
 
-    .then(function (data) {
+  .then(function (data) {
 
-        for (var i=0; i<10; i++) {
+    for (var i = 0; i < 10; i++) {
 
-            var title = data.articles[i].title
-            var author = data.articles[i].author
-            var content = data.articles[i].content
-            console.log(title,author,content)
-        }
-      });
+      var title = data.articles[i].title
+      var author = data.articles[i].author
+      var content = data.articles[i].content
+      console.log(title, author, content)
+    }
+  });
 
 
 //This is function to read news everytime a read button is clicked more coments will be addid ========= Getnet's code starts here=============
-var read=document.querySelectorAll(".read");
-var newsAudio =new Audio();
-var audioUrl="";
-var nxtAudioUrl="";
-var currntAudioIndex=0;
-var pausedAudioTime=0
-var newsAudioEnd=0;
+var read = document.querySelectorAll(".read");
+var newsAudio = new Audio();
+var audioUrl = "";
+var nxtAudioUrl = "";
+var currntAudioIndex = 0;
+var pausedAudioTime = 0
+var newsAudioEnd = 0;
 //This function will read aloud the content of news cadrd where clicked button located . content element will be selected based on parent of button and the child of that parent
 function readAloud() {
-  setInterval(function() {
+  setInterval(function () {
     console.log(newsAudioEnd);
-    if (newsAudio.currentTime==newsAudioEnd) {
+    if (newsAudio.currentTime == newsAudioEnd) {
       newsAudio.pause();
-      read[currntAudioIndex].textContent='read'
-    }else{
-      newsAudioEnd=newsAudio.currentTime;
-     
+      read[currntAudioIndex].textContent = 'read'
+    } else {
+      newsAudioEnd = newsAudio.currentTime;
+
       console.log(newsAudio);
     }
   }, 1000)
-  
+
   for (let i = 0; i < read.length; i++) {
-     
-    read[i].addEventListener("click", ()=>{
-       nxtAudioUrl="./file_example_MP3_1MG"+ read[i].parentElement.children[0].textContent+ ".mp3";
-      if (nxtAudioUrl===audioUrl||audioUrl==="") {
+
+    read[i].addEventListener("click", () => {
+      nxtAudioUrl = "./file_example_MP3_1MG" + read[i].parentElement.children[0].textContent + ".mp3";
+      if (nxtAudioUrl === audioUrl || audioUrl === "") {
         console.log(newsAudio);
         if (newsAudio.paused) {
-          audioUrl="./file_example_MP3_1MG"+ read[i].parentElement.children[0].textContent+ ".mp3";
-          currntAudioIndex=i;
-          newsAudio =new Audio(audioUrl);
+          audioUrl = "./file_example_MP3_1MG" + read[i].parentElement.children[0].textContent + ".mp3";
+          currntAudioIndex = i;
+          newsAudio = new Audio(audioUrl);
           newsAudio.play();
-          newsAudio.currentTime=pausedAudioTime;
-          read[i].textContent="Pouse";
+          newsAudio.currentTime = pausedAudioTime;
+          read[i].textContent = "Pouse";
         } else {
           newsAudio.pause();
-          pausedAudioTime=newsAudio.currentTime;
-      
-          read[i].textContent="read"
+          pausedAudioTime = newsAudio.currentTime;
+
+          read[i].textContent = "read"
         }
       } else {
         newsAudio.pause();
-        read[currntAudioIndex].textContent='read'
-         
-        audioUrl="./file_example_MP3_1MG"+ read[i].parentElement.children[0].textContent+ ".mp3";
-        currntAudioIndex=i;
-        newsAudio =new Audio(audioUrl);
+        read[currntAudioIndex].textContent = 'read'
+
+        audioUrl = "./file_example_MP3_1MG" + read[i].parentElement.children[0].textContent + ".mp3";
+        currntAudioIndex = i;
+        newsAudio = new Audio(audioUrl);
         newsAudio.play();
-        
-        read[i].textContent="Pouse";
+
+        read[i].textContent = "Pouse";
       }
     });
-    
+
   }
 }
 readAloud();
