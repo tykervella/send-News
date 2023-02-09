69d49bb2be2821b7de59c48bdf907fff0fb66cf9
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-02-08 22:02:34 -0800

    added eventlistener and fetch api

--- assets/script.js
+++ assets/script.js
@@ -64,88 +64,135 @@ var categorystring = "category=" + category + "&"
 
 // url to show top headlines in specific categories
 var topheadlinesurl = "https://newsapi.org/v2/top-headlines?" +
-          countrystring +
-          categorystring +
-          searchstring +
-          "pagesize=10&" +
-          "apiKey=9db76ac16d454b918d7c994623816b9b"
+  countrystring +
+  categorystring +
+  searchstring +
+  "pagesize=10&" +
+  "apiKey=9db76ac16d454b918d7c994623816b9b"
 
 var req = new Request(topheadlinesurl);
 
 // grab the API url and manipulates the data 
 fetch(req)
-    .then(function(response) {
-        return response.json();
-    })
+  .then(function (response) {
+    return response.json();
+  })
 
-    .then(function (data) {
-      
+  .then(function (data) {
 
-        for (var i=0; i<data.articles.length; i++) {
 
-            var title = data.articles[i].title
-            var author = data.articles[i].author
-            var content = data.articles[i].content
-            console.log(title,author,content)
-        }
-      });
+    for (var i = 0; i < data.articles.length; i++) {
+
+      var title = data.articles[i].title
+      var author = data.articles[i].author
+      var content = data.articles[i].content
+      console.log(title, author, content)
+    }
+  });
 
 
 //This is function to read news everytime a read button is clicked more coments will be addid ========= Getnet's code starts here=============
-var read=document.querySelectorAll(".read");
-var newsAudio =new Audio();
-var audioUrl="";
-var nxtAudioUrl="";
-var currntAudioIndex=0;
-var pausedAudioTime=0
-var newsAudioEnd=0;
+var read = document.querySelectorAll(".read");
+var newsAudio = new Audio();
+var audioUrl = "";
+var nxtAudioUrl = "";
+var currntAudioIndex = 0;
+var pausedAudioTime = 0
+var newsAudioEnd = 0;
 //This function will read aloud the content of news cadrd where clicked button located . content element will be selected based on parent of button and the child of that parent
 function readAloud() {
-  setInterval(function() {
+  setInterval(function () {
     console.log(newsAudioEnd);
-    if (newsAudio.currentTime==newsAudioEnd) {
+    if (newsAudio.currentTime == newsAudioEnd) {
       newsAudio.pause();
-      read[currntAudioIndex].textContent='read'
-    }else{
-      newsAudioEnd=newsAudio.currentTime;
-     
+      read[currntAudioIndex].textContent = 'read'
+    } else {
+      newsAudioEnd = newsAudio.currentTime;
+
       console.log(newsAudio);
     }
   }, 1000)
-  
+
   for (let i = 0; i < read.length; i++) {
-     
-    read[i].addEventListener("click", ()=>{
-       nxtAudioUrl="./file_example_MP3_1MG"+ read[i].parentElement.children[0].textContent+ ".mp3";
-      if (nxtAudioUrl===audioUrl||audioUrl==="") {
+
+    read[i].addEventListener("click", () => {
+      nxtAudioUrl = "./file_example_MP3_1MG" + read[i].parentElement.children[0].textContent + ".mp3";
+      if (nxtAudioUrl === audioUrl || audioUrl === "") {
         console.log(newsAudio);
         if (newsAudio.paused) {
-          audioUrl="./file_example_MP3_1MG"+ read[i].parentElement.children[0].textContent+ ".mp3";
-          currntAudioIndex=i;
-          newsAudio =new Audio(audioUrl);
+          audioUrl = "./file_example_MP3_1MG" + read[i].parentElement.children[0].textContent + ".mp3";
+          currntAudioIndex = i;
+          newsAudio = new Audio(audioUrl);
           newsAudio.play();
-          newsAudio.currentTime=pausedAudioTime;
-          read[i].textContent="Pouse";
+          newsAudio.currentTime = pausedAudioTime;
+          read[i].textContent = "Pouse";
         } else {
           newsAudio.pause();
-          pausedAudioTime=newsAudio.currentTime;
-      
-          read[i].textContent="read"
+          pausedAudioTime = newsAudio.currentTime;
+
+          read[i].textContent = "read"
         }
       } else {
         newsAudio.pause();
-        read[currntAudioIndex].textContent='read'
-         
-        audioUrl="./file_example_MP3_1MG"+ read[i].parentElement.children[0].textContent+ ".mp3";
-        currntAudioIndex=i;
-        newsAudio =new Audio(audioUrl);
+        read[currntAudioIndex].textContent = 'read'
+
+        audioUrl = "./file_example_MP3_1MG" + read[i].parentElement.children[0].textContent + ".mp3";
+        currntAudioIndex = i;
+        newsAudio = new Audio(audioUrl);
         newsAudio.play();
-        
-        read[i].textContent="Pouse";
+
+        read[i].textContent = "Pouse";
       }
     });
-    
+
   }
 }
 //readAloud();
 //============================Getenet's code ends here===================================================
+
+//=============================Jimmys code start=======================================
+//add event listener to searchBtn and runs function when clicked
+document.getElementById("searchBtn").addEventListener("click", function () {
+  console.log(document.getElementById("searchString").value);
+  console.log(document.getElementById("selectCat").value);
+
+  var searchinput = document.getElementById("searchString").value
+  var searchstring = 'q=' + searchinput + '&'
+
+  // possible categories
+  //business, entertainment, general, health, science, sports, technology
+  var category = document.getElementById("selectCat").value
+  var categorystring = "category=" + category + "&"
+
+  // url to show top headlines in specific categories
+  var topheadlinesurl = "https://newsapi.org/v2/top-headlines?" +
+    countrystring +
+    categorystring +
+    searchstring +
+    "pagesize=10&" +
+    "apiKey=9db76ac16d454b918d7c994623816b9b"
+
+  var req = new Request(topheadlinesurl);
+
+  // grab the API url and parses response through JSON
+  fetch(req)
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then(function (data) {
+      document.getElementById("mainContent").innerHTML = ""
+    
+      for (var i = 0; i < data.articles.length; i++) {
+
+        var title = data.articles[i].title
+        var author = data.articles[i].author
+        var content = data.articles[i].content
+        console.log(title, author, content)
+
+        // displays article fetched and displays to mainContent
+        // document.getElementById("mainContent").innerHTML += data.articles[i].content
+      }
+    });
+});
+//=============================================Jimmys code end============================================
